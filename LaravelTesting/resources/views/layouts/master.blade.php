1b93cd32e8db21c1ff6df54b604e45e403cc7890
StefdeKramer <0908034@hr.nl>
2016-09-25 18:55:41 +0200

    Starthomepage + database user opslaan
    Aantal stukken tekst als filler voor homepage waar je niet hoeft te zijn
    inloggen, en je kan als user signuppen en dat wordt opgeslagen in de
    database en wachtwoord wordt direct gehasssschht

--- LaravelTesting/resources/views/layouts/master.blade.php
+++ LaravelTesting/resources/views/layouts/master.blade.php
@@ -6,10 +6,11 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
           integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
+
 </head>
 <body>
 @include('includes.header')
-<div class="containter">
+<div class="container">
 
 
 
--- LaravelTesting/resources/views/welcome.blade.php
+++ LaravelTesting/resources/views/welcome.blade.php
@@ -4,27 +4,52 @@
     My bitches
 @endsection
 
-@section('Image')
-    <div class="row">
-        <div class="col-md-2">
-            <h2>Sportsy</h2>
-            <p>Welcome, This application makes selling/rating sports products easy and simple. To participate you simply
-                have to make an account and posts some products of yourself.</p>
-        </div>
-
-    </div>
-@endsection
-
 
 @section('content')
-    <div class="row">
-        <div class="col-md-2">
-            <h2>Sportsy</h2>
-            <p>Welcome, This application makes selling/rating sports products easy and simple. To participate you simply
-                have to make an account and posts some products of yourself.</p>
-        </div>
+    <div class="text-left">
+
+        <h2>Sportsy</h2>
+        <p>Welcome, This application makes selling/rating sports products easy and simple. To participate you simply
+            have to make an account and posts some products of yourself. Lorem ipsum Lorem ipsum dolor sit amet,
+            consectetur adipiscing elit. Cras ultrices ut ante eget tempus. Cras accumsan finibus lectus, id ornare nibh
+            auctor a. Fusce iaculis elit diam, id ullamcorper mi blandit eu. Quisque quis lacus id sapien elementum
+            porta sed eu erat. Integer condimentum ipsum tincidunt magna faucibus sodales. Nullam in vestibulum massa.
+            Nunc non urna in lorem interdum sollicitudin. Maecenas ultrices leo sed magna accumsan egestas. Suspendisse
+            potenti. Curabitur augue nulla, pharetra ac tincidunt ac, fringilla id urna. Donec quis quam id lectus
+            scelerisque egestas non in turpis. Proin eu tellus ornare, pretium sapien at, viverra neque. Morbi pharetra
+            nibh nec mauris bibendum hendrerit. Fusce convallis tortor imperdiet pretium scelerisqu </p>
+    </div>
+
+    <div class="text-left">
+
+        <h2>filler</h2>
+        <p> Dit is een geimproviseerde tekst opvulling. Deze is gemaakt door mij stef en mij alleen. Ik ben de
+            enige die hier heeft getypt en ik ratel wat door zodat ik wat meer tekst heb staan. Ik houd niet van
+            lorum ipsum maar van ipsum lorem</p>
+    </div>
+    <div class="text-left">
+        <h2>filler</h2>
+        <p> Dit is een geimproviseerde tekst opvulling. Deze is gemaakt door mij stef en mij alleen. Ik ben de
+            enige die hier heeft getypt en ik ratel wat door zodat ik wat meer tekst heb staan. Ik houd niet van
+            lorum ipsum maar van ipsum lorem</p>
+
 
     </div>
+    <div class="text-left">
+
+        <h2>filler</h2>
+        <p> Dit is een geimproviseerde tekst opvulling. Deze is gemaakt door mij stef en mij alleen. Ik ben de
+            enige die hier heeft getypt en ik ratel wat door zodat ik wat meer tekst heb staan. Ik houd niet van
+            lorum ipsum maar van ipsum lorem</p>
+    </div>
+    <div class="text-left">
+
+        <h2>filler</h2>
+        <p> Dit is een geimproviseerde tekst opvulling. Deze is gemaakt door mij stef en mij alleen. Ik ben de
+            enige die hier heeft getypt en ik ratel wat door zodat ik wat meer tekst heb staan. Ik houd niet van
+            lorum ipsum maar van ipsum lorem</p>
+    </div>
+
     <div class="row">
         <div class="col-md-6">
 
